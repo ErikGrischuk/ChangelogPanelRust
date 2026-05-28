6f78985fdeaecf67eb23497e34eb4a43cc087ece
Erik <33401464+ErikGrischuk@users.noreply.github.com>
2026-05-28 21:40:32 +0200

    Add files via upload

---
+++ ClientChangelogButton.cs
@@ -0,0 +1,20 @@
+using System;
+using Rust.UI;
+using UnityEngine;
+
+public class ChangelogButton : MonoBehaviour
+{
+	public ChangelogButton()
+	{
+		throw null;
+	}
+
+	private void Update()
+	{
+		throw null;
+	}
+
+	public RustButton Button;
+
+	public CanvasGroup CanvasGroup;
+}
---
+++ ClientChangelogPanel.cs
@@ -0,0 +1,121 @@
+using System;
+using Rust.UI;
+using UnityEngine;
+
+public class ChangelogPanel : MonoBehaviour
+{
+	private void %3895a030b0cbd3524f3d49243862615b26e9bb71(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	private string %b2d1d9a74ad8864e4f14b441d7696b85fadf798e(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	private void %3ea91d9ae19ea3024e472c0673c68b8340f4da22(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	private void %a2f2d0624e43724e7710dc2cf78e97325d020ae6(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	private string %b251f02400ddc4918f4f0648561d1d4a05eca3da(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	private string %04ff56eac0c07d6e1d4b18c27e4b01637a315aee(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	private string %b0e4bcdbadbbdfd5dd154875860fa5a8b9c00c2f(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	public void Close()
+	{
+		throw null;
+	}
+
+	private string %a076c3867b9948c56bb251a531f6170e6e00f2a1(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	private string %cb56c8895a27fa9b4e56a3259e229a1728c22095(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	private void %89033c80b71685cf41920d0efadabd9335dd4ed4(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	public ChangelogPanel()
+	{
+		throw null;
+	}
+
+	private void %7f1263236c2675e24ee3b2cb8f799938c15c8961(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	private void %1842182d1e12f0eb7b294460950ae7445539ed01(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	private string %40a89384bca019bff598834ca275f8e2df61e476(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	public void Open()
+	{
+		throw null;
+	}
+
+	public void Rebuild()
+	{
+		throw null;
+	}
+
+	private void %652a6516adac28fdccd474c4132a2d3af2bf7b1d(ChangelogPanel.ChangelogSection %d62d4726e4e6be1d6d26eb6cb8b2d89d42febec4, BaseGameMode %960b2bbe20526e6d04e87b945529dabeba277d5b)
+	{
+		throw null;
+	}
+
+	public GameObject AddedHeader;
+
+	public GameObject RemovedHeader;
+
+	public GameObject ChangedHeader;
+
+	public RustText AddedContent;
+
+	public RustText RemovedContent;
+
+	public RustText ChangedContent;
+
+	public RectTransform MainPanelRectTransform;
+
+	public RectTransform ContentPanelRectTransform;
+
+	private BaseGameMode %872833b3572b41d85dbb0241b19dfbfc66da34da;
+
+	private enum ChangelogSection
+	{
+		Added,
+		Removed,
+		Changed
+	}
+}
